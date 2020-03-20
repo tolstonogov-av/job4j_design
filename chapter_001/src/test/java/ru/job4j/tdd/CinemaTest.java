1b1375c17f3195c4e4d1617c23aeaefb92319771
tolstinator <tolstonogov@mail.ru>
2020-03-20 09:30:57 +0700

    3. Шаблонизатор. [#855].

--- chapter_001/src/test/java/ru/job4j/tdd/CinemaTest.java
+++ chapter_001/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -8,6 +8,16 @@ import static org.junit.Assert.*;
 
 public class CinemaTest {
     /**
+     * Seat with row == 0 and column == 0 doesn't exist.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void illegalDataOfTicket() {
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket = new Cinema3D().buy(new AccountCinema(), 0, 0, date);
+    }
+
+    /**
      * When buying a ticket, the theater issues a new ticket.
      */
     @Test
@@ -18,6 +28,15 @@ public class CinemaTest {
         date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         assertNull(ticket);
+    }
+
+    /**
+     * For example, a session that has already ended.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void illegalSessionToAdd() {
+        Session session = new Session3D();
+        new Cinema3D().add(session);
     }
 
     /**
@@ -32,7 +51,7 @@ public class CinemaTest {
     }
 
     /**
-     * When a new session is added, it is possible to find it in the storage by predicate.
+     * When a new sessions is added, it is possible to find it in the storage by predicate.
      */
     @Test
     public void findByPredicate() {
